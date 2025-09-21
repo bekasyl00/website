13b574da4d01320621e72df0ff00e4618b1e13fa
Global Username <globalemail@example.com>
2025-09-21 18:38:16 +0500

    lastest change

--- beka/settings.py
+++ beka/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+   'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -108,9 +109,12 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 STATICFILES_DIRS=[
+    
     os.path.join(BASE_DIR,'static')
+    
    
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 # ]
